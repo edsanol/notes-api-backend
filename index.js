828f2ef3aed0c3dffda0082376edbea0c4c25648
Edinael <edsanol99@gmail.com>
2022-04-16 00:27:25 -0500

    Add frontend

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 app.use(requestLogger)
 
@@ -81,10 +82,10 @@ app.post('/api/notes', (request, response) => {
     }
   
     const note = {
+        id: generateId(),
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId(),
     }
   
     notes = notes.concat(note)
